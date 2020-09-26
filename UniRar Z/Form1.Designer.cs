9b94d2325ad2f0510b16251df5baa8cfe50dda88
urz9999 <urz9999@gmail.com>
2020-09-26 19:41:28 +0200

    v1.0

---
+++ UniRar Z/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace UniRar_Z
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.localeCombobox = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.actionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeUniRarZToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.listView = new System.Windows.Forms.ListView();
+            this.File = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Ext = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.extractBTN = new System.Windows.Forms.Button();
+            this.loadBTN = new System.Windows.Forms.Button();
+            this.loadCmpressedFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extractDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // localeCombobox
+            // 
+            this.localeCombobox.FormattingEnabled = true;
+            this.localeCombobox.Items.AddRange(new object[] {
+            "Japan : ja-JP",
+            "China : zh-CN",
+            "Korea : ko-KR",
+            "India : hi-IN",
+            "Italy : it-IT",
+            "United States : en-US",
+            "Egypt : ar-EG"});
+            this.localeCombobox.Location = new System.Drawing.Point(15, 66);
+            this.localeCombobox.Name = "localeCombobox";
+            this.localeCombobox.Size = new System.Drawing.Size(303, 21);
+            this.localeCombobox.TabIndex = 2;
+            this.localeCombobox.Text = "Japan : ja-JP";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actionsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(330, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // actionsToolStripMenuItem
+            // 
+            this.actionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadCmpressedFileToolStripMenuItem,
+            this.extractDataToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.closeUniRarZToolStripMenuItem});
+            this.actionsToolStripMenuItem.Name = "actionsToolStripMenuItem";
+            this.actionsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.actionsToolStripMenuItem.Text = "Actions";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(200, 6);
+            // 
+            // closeUniRarZToolStripMenuItem
+            // 
+            this.closeUniRarZToolStripMenuItem.Name = "closeUniRarZToolStripMenuItem";
+            this.closeUniRarZToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.closeUniRarZToolStripMenuItem.Text = "Close UniRar Z";
+            this.closeUniRarZToolStripMenuItem.Click += new System.EventHandler(this.closeUniRarZToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1.Select Original Locale\r\n(note: restart the program for different locale)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "2.Select file to extract";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 351);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "3.Extract your data";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(15, 435);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(303, 12);
+            this.progressBar.TabIndex = 8;
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.File,
+            this.Size,
+            this.Ext});
+            this.listView.FullRowSelect = true;
+            this.listView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView.Location = new System.Drawing.Point(15, 183);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.ShowGroups = false;
+            this.listView.Size = new System.Drawing.Size(303, 165);
+            this.listView.SmallImageList = this.imageList1;
+            this.listView.TabIndex = 9;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // File
+            // 
+            this.File.Text = "File";
+            this.File.Width = 179;
+            // 
+            // Size
+            // 
+            this.Size.Text = "Size";
+            this.Size.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Size.Width = 80;
+            // 
+            // Ext
+            // 
+            this.Ext.Text = "Ext";
+            this.Ext.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Ext.Width = 40;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1447393984_OK.png");
+            this.imageList1.Images.SetKeyName(1, "1447394030_No_entry.png");
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Image = global::UniRar_Z.Properties.Resources.f34434cf;
+            this.stopBtn.Location = new System.Drawing.Point(168, 369);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(150, 60);
+            this.stopBtn.TabIndex = 10;
+            this.stopBtn.Text = "Stop Process";
+            this.stopBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // extractBTN
+            // 
+            this.extractBTN.Image = global::UniRar_Z.Properties.Resources.extract_love1;
+            this.extractBTN.Location = new System.Drawing.Point(15, 369);
+            this.extractBTN.Name = "extractBTN";
+            this.extractBTN.Size = new System.Drawing.Size(150, 60);
+            this.extractBTN.TabIndex = 1;
+            this.extractBTN.Text = "Extract Data";
+            this.extractBTN.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.extractBTN.UseVisualStyleBackColor = true;
+            this.extractBTN.Click += new System.EventHandler(this.extractBTN_Click);
+            // 
+            // loadBTN
+            // 
+            this.loadBTN.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.loadBTN.Image = global::UniRar_Z.Properties.Resources.Folder_Archive_zip_icon;
+            this.loadBTN.Location = new System.Drawing.Point(15, 117);
+            this.loadBTN.Name = "loadBTN";
+            this.loadBTN.Size = new System.Drawing.Size(303, 60);
+            this.loadBTN.TabIndex = 0;
+            this.loadBTN.Text = "Load Compressed File(s)";
+            this.loadBTN.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.loadBTN.UseVisualStyleBackColor = true;
+            this.loadBTN.Click += new System.EventHandler(this.loadBTN_Click);
+            // 
+            // loadCmpressedFileToolStripMenuItem
+            // 
+            this.loadCmpressedFileToolStripMenuItem.Image = global::UniRar_Z.Properties.Resources.Folder_Archive_zip_icon;
+            this.loadCmpressedFileToolStripMenuItem.Name = "loadCmpressedFileToolStripMenuItem";
+            this.loadCmpressedFileToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.loadCmpressedFileToolStripMenuItem.Text = "Load Compressed File(s)";
+            this.loadCmpressedFileToolStripMenuItem.Click += new System.EventHandler(this.loadCmpressedFileToolStripMenuItem_Click);
+            // 
+            // extractDataToolStripMenuItem
+            // 
+            this.extractDataToolStripMenuItem.Image = global::UniRar_Z.Properties.Resources.extract_love1;
+            this.extractDataToolStripMenuItem.Name = "extractDataToolStripMenuItem";
+            this.extractDataToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.extractDataToolStripMenuItem.Text = "Extract Data";
+            this.extractDataToolStripMenuItem.Click += new System.EventHandler(this.extractDataToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 451);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.localeCombobox);
+            this.Controls.Add(this.extractBTN);
+            this.Controls.Add(this.loadBTN);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UniRar Z - Unicode File Decompressor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadBTN;
+        private System.Windows.Forms.Button extractBTN;
+        private System.Windows.Forms.ComboBox localeCombobox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem actionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadCmpressedFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem extractDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem closeUniRarZToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader File;
+        private System.Windows.Forms.ColumnHeader Size;
+        private System.Windows.Forms.ColumnHeader Ext;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button stopBtn;
+    }
+}
+
---
+++ UniRar Z/Form1.cs
@@ -0,0 +1,192 @@
+﻿using SharpCompress.Archive;
+using SharpCompress.Common;
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace UniRar_Z
+{
+    public partial class Form1 : Form
+    {
+        private CultureInfo ci;
+        private string[] zipLocation;
+        private string selectedPath;
+        private BackgroundWorker bW;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadCmpressedFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void extractDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            extractData();
+        }
+
+        private void closeUniRarZToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("UniRar Z - copyright 2015, Alessandro Gaggia\n\nFree to use, thanks to SharpCompress library for decompressing capabilities.\nSharpCompress.dll:\nhttps://sharpcompress.codeplex.com/\nAll rights to respective owners.");
+        }
+
+        private void loadBTN_Click(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void extractBTN_Click(object sender, EventArgs e)
+        {
+            extractData();
+        }
+
+        private void loadData() 
+        {
+            listView.Items.Clear();
+            selectedPath = "";
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Compressed Archives (*.zip;*.rar;*.tar;*.tar.gz;*.7z)|*.zip;*.rar;*.tar;*.tar.gz;*.7z|All files (*.*)|*.*";
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == DialogResult.OK) 
+            {
+                zipLocation = ofd.FileNames;
+                foreach (string zip in zipLocation) {
+                    FileInfo fi = new FileInfo(zip);
+                    ListViewItem lvi = new ListViewItem(new string[]{zip, HumanReadableSize(fi.Length), fi.Extension});
+                    lvi.ImageIndex = 1;
+                    listView.Items.Add(lvi);
+                }
+            }
+            ofd.Dispose();
+        }
+
+        private void extractData() {
+            if (zipLocation.Length > 0) 
+            {
+                progressBar.Value = 0;
+
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                if(fbd.ShowDialog() == DialogResult.OK) 
+                {
+                    selectedPath = fbd.SelectedPath;
+
+                    ci = new CultureInfo(localeCombobox.Text.Split(new string[] { " : " }, StringSplitOptions.None)[1].Trim());
+
+                    bW = new BackgroundWorker
+                    {
+                        WorkerReportsProgress = true,
+                        WorkerSupportsCancellation = true
+                    };
+                    bW.DoWork += new DoWorkEventHandler(bW_DoWork);
+                    bW.ProgressChanged += new ProgressChangedEventHandler(bW_ProgressChanged);
+                    bW.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bW_RunWorkerCompleted);
+                    bW.RunWorkerAsync();
+                }
+                fbd.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("Select at least one compressed archive.");
+            }
+        }
+
+        private void bW_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar.Value = progressBar.Maximum;
+            MessageBox.Show("File Extracted successfully.");
+        }
+
+        private void bW_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value += 1;
+        }
+
+        private void bW_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Application.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+
+            BackgroundWorker worker = sender as BackgroundWorker;
+            foreach (string currentZip in zipLocation)
+            {
+                if (worker.CancellationPending == false)
+                {
+                    int count = ArchiveFactory.Open(currentZip).Entries.Count();
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        progressBar.Maximum = count;
+                        progressBar.Value = 0;
+                    });
+
+
+                    using (Stream stream = System.IO.File.OpenRead(currentZip))
+                    {
+                        var reader = ArchiveFactory.Open(stream);
+                        foreach (var entry in reader.Entries)
+                        {
+                            if (!entry.IsDirectory)
+                            {
+                                entry.WriteToDirectory(selectedPath, ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite);
+                            }
+                            worker.ReportProgress(0);
+                        }
+                    }
+
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        foreach (ListViewItem lvi in listView.Items)
+                        {
+                            if (lvi.Text == currentZip)
+                            {
+                                lvi.ImageIndex = 0;
+                            }
+                        }
+                    });
+                }
+                else {
+                    
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        MessageBox.Show("Operation aborted.");                   
+                    });
+                    return;
+                }
+            }
+        }
+
+        private string HumanReadableSize(long bytes) {
+            string[] sizes = { "B", "KB", "MB", "GB" };
+            double len = bytes;
+            int order = 0;
+            while (len >= 1024 && order + 1 < sizes.Length)
+            {
+                order++;
+                len /= 1024;
+            }
+
+            // Adjust the format string to your preferences. For example "{0:0.#}{1}" would
+            // show a single decimal place, and no space.
+            return String.Format("{0:0.##} {1}", len, sizes[order]);
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            if (bW != null)
+                bW.CancelAsync();
+        }        
+    }
+}
